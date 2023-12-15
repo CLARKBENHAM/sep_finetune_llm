deddc186f48bb854f512a491a62a09dd8847f1b9
CLARKBENHAM <clark.benham@gmail.com>
2023-12-15 00:27:18 -0500

    pick between tokens to use

---
+++ chars_tokenized_separately.py
@@ -0,0 +1,11 @@
+ord_use_between = [
+    0,  # most unique
+    8,  # delete char
+    11,  # min matching  re.match('\s', chr(i)) in self_sync_ords
+    # also min(works3) i.ie not changing ss1, ss2, h1
+    190,  # max(works3)
+    192,  # min matching regex.match('[^\r\n\p{L}\p{N}]?\p{L}+' in self_sync_ords
+    # also min([i for n,i in num_sep_1_presuf if n == num_sep_1_presuf[1][0]])
+    255,  # max([i for n,i in num_sep_1_presuf if n == num_sep_1_presuf[1][0]])
+    1000,  # big and round, for randomness. not self_sync nor ascii
+]
