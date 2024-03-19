fda26bb73f3d88b93942db6ba1e388dccd3fe36f
CLARKBENHAM <clark.benham@gmail.com>
2024-03-18 20:01:33 -0700

    haiku vs opus; random unicode

--- temp_scripts/test_tokenization.py
+++ temp_scripts/test_tokenization.py
@@ -1,10 +1,11 @@
 # %%
-
+import numpy as np
 import os
 import random
 import re
 import code
 from itertools import zip_longest
+import string
 
 from anthropic import Anthropic, AsyncAnthropic
 
@@ -15,8 +16,8 @@ an_client_sync = Anthropic()
 
 # %%
 # tokenizers based on different models aren't the same
-# different when words are joined by random unicode
-from itertools import zip_longest
+# haiku and opus are the same, sonnet different on: s4 didn't repeat enough unicode, s5 broke words into bigger pieces
+# sometimes is different if combine ascii and unicode; for s10 haiku had extra ' ' on end but ran again and fine
 
 s1 = (
     "We hold these truths to be self-evident, that all men are created equal, that they are"
@@ -31,16 +32,55 @@ s3 = """Ↄ⊙◵⳺⸬⩋ⷒ⊨⃺ⴾ❭⬫⼀⠹⍓⩁⤋┝⢫⁴⨂☍⭬➖
 s4 = "".join([c for i, j in zip_longest(s1.split(" "), list(s3), fillvalue="") for c in (i, j)])
 s5 = "".join([c for i, j in zip_longest(s2.split(" "), list(s3), fillvalue="") for c in (i, j)])
 
+# random char words
+s6 = " ".join(
+    "".join(random.choice(string.ascii_letters) for _ in range(random.randint(1, 10)))
+    for _ in range(20)
+)
+s7 = " ".join(
+    "".join(random.choice(string.ascii_letters) for _ in range(random.randint(1, 10)))
+    for _ in range(20)
+)
 
-for ix, s in enumerate((s1, s2, s3, s4, s5)):
+# random ascii letters
+s8 = "".join(random.choice(string.ascii_letters) for _ in range(100))
+s9 = "".join(random.choice(string.ascii_letters) for _ in range(100))
+
+unicode_range = [0x0000, 0xD7FF]
+
+# random if ascii letters or unicode
+s10 = "".join(
+    (
+        random.choice(string.ascii_letters)
+        if random.random() < 0.5
+        else chr(np.random.randint(*unicode_range))
+    )
+    for _ in range(100)
+)
+s11 = "".join(
+    (
+        random.choice(string.ascii_letters)
+        if random.random() < 0.5
+        else chr(np.random.randint(*unicode_range))
+    )
+    for _ in range(100)
+)
+
+# random ascii
+s12 = "".join(chr(random.choice(range(256))) for _ in range(100))
+s13 = "".join(chr(random.choice(range(256))) for _ in range(100))
+
+# for ix, s in enumerate([s1, s2, s3, s4, s5,s6, s7, s8,s9,s10, s11,s12, s13]):
+for ix, s in enumerate([s10]):
     o1 = tokenize_text(an_client_async, s, model="claude-3-opus-20240229")
     o2 = tokenize_text(an_client_async, s, model="claude-3-sonnet-20240229")
     o3 = tokenize_text(an_client_async, s, model="claude-3-haiku-20240307")
-    good = all([o1 == o2, o2 == o1, o1 == o3])
-    if not good:
+    good = [o1 == o2, o2 == o3, o1 == o3]
+    if not all(good):
         print(o1, o2, o3, sep="\n")
-    assert good
-    print(f"finished {ix}")
+        print(ix, good)
+        assert False, s
+    print(f"finished {ix},              {s}")
 
 
 # %% Anthropic can print out all the chars in each token.
